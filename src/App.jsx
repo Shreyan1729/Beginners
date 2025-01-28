fbff58b497cdcabe04a3afedd2acb5be53a483da
Shreyan1729 <shreyandeyrudra@gmail.com>
2025-01-28 16:34:12 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import "./styles/App.css";
+import "./styles/Responsive.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      App
+      <Footer />
+    </>
+  );
 };
 
 export default App;
